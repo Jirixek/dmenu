35168c97a47cf796eae314d6dbd9c9aaf96fad1c
Jiří Szkandera <jirik.sz@gmail.com>
2021-01-05 02:42:05 +0100

    Enable color emoji

--- config.h
+++ config.h
@@ -3,7 +3,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=12"
+	"monospace:size=12",
+	"Noto Color Emoji:pixelsize=12"
 };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
